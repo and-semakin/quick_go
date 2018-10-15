311b9fd3f16e4cc0ec0076233f5d0d8b0d046c20
Andrey Semakin <and-semakin@ya.ru>
2018-10-15 14:32:12 +0500

    Load game from server and display

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Game from './containers/Game/Game';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Game />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Route path='/game/:link' component={Game} />
+          <Route exact path='/' render={() => (
+            <div>
+              <h1>New game</h1>
+              <p>Nothing here yet.</p>
+              <Link to="/game/a0eb563da2d278e448943b4372f831a6">Go game</Link>
+            </div>
+          )} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- client/src/containers/Game/Game.js
+++ client/src/containers/Game/Game.js
@@ -2,35 +2,105 @@ import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 
 import Goban from '../../components/Goban/Goban';
+import axios from 'axios';
 
 import { xy2n, findGroup, captureGroup, findLiberties, isGobanEqual } from '../../go_helpers';
 
 class Game extends Component {
     state = {
+        isBlack: undefined,
         move: 0,
+        link: undefined,
+        finished: undefined,
         capturedWhite: 0,
         capturedBlack: 0,
-        gobanSize: 9,
-        gobanHistory: [],
+        gobanSize: 0,
+        gobanHistory: [[]],
     }
 
-    componentWillMount() {
-        this.setState({
-            gobanHistory: [[...Array(this.state.gobanSize)].map(e => Array(this.state.gobanSize))],
-        })
+    componentDidMount() {
+        console.log('Requesting game info...');
+        const link = this.props.match.params.link;
+        axios.get(`/api/game/${link}`)
+            .then(response => {
+                console.log(response);
+                const {
+                    finished,
+                    gobanSize,
+                    isBlack,
+                    move,
+                    moves
+                } = response.data;
+
+                const gobanHistory = [[...Array(gobanSize)].map(e => Array(gobanSize))];
+                let goban = gobanHistory[0];
+                let capturedBlack = 0;
+                let capturedWhite = 0;
+
+                console.log(-1, gobanHistory);
+
+                moves.forEach((move, index) => {
+                    console.log(index, gobanHistory);
+
+                    const {
+                        order,
+                        pass,
+                        x, y
+                    } = move;
+                    const {goban: newGoban, captured, error} = this.updateGoban(goban, order, x, y, pass);
+                    if (error) {
+                        console.log(error);
+                    }
+                    if (index % 2 === 0) {
+                        capturedBlack = capturedBlack + captured;
+                    } else {
+                        capturedWhite = capturedWhite + captured;
+                    }
+                    gobanHistory.push(newGoban);
+                    goban = newGoban;
+                });
+
+                this.setState({
+                    finished,
+                    gobanSize,
+                    isBlack,
+                    capturedBlack,
+                    capturedWhite,
+                    move,
+                    gobanHistory,
+                })
+            })
+            .catch(error => {
+                console.log(error);
+            })
     }
 
-    updateGoban = (goban, x, y) => {
-        goban[x][y] = this.state.move;
+    updateGoban = (sourceGoban, move, x, y, pass) => {
+        const gobanSize = sourceGoban.length;
+
+        // deep copy of current goban state
+        const goban = sourceGoban.map(gobanRow => gobanRow.slice());
+
+        // pass
+        if (pass) {
+            return {goban: goban, captured: 0};
+        }
+
+        // the place is taken
+        if (goban[x][y] !== undefined) {
+            return {goban: undefined, captured: undefined, error: 'The place is taken'};
+        }
+
+        goban[x][y] = move;
 
         let captured = 0;
 
-        const updatedGroup = findGroup(goban, xy2n(x, y, this.state.gobanSize));
+        const updatedGroup = findGroup(goban, xy2n(x, y, gobanSize));
 
-        for (let i = 0; i < this.state.gobanSize; i++) {
-            for (let j = 0; j < this.state.gobanSize; j++) {
-                const n = xy2n(i, j, this.state.gobanSize);
-                if (goban[i][j] === undefined || 
+        for (let i = 0; i < gobanSize; i++) {
+            for (let j = 0; j < gobanSize; j++) {
+                const n = xy2n(i, j, gobanSize);
+                if (goban[i][j] === undefined ||
                     updatedGroup.includes(n)) {
                     continue;
                 }
@@ -42,37 +112,22 @@ class Game extends Component {
             }
         }
 
-        const isSuicideMove = findLiberties(goban, xy2n(x, y, this.state.gobanSize)).size === 0;
+        const isSuicideMove = findLiberties(goban, xy2n(x, y, gobanSize)).size === 0;
         if (isSuicideMove) {
-            return {goban: false, captured: 0};
+            return { goban: false, captured: 0, error: 'Suicide detected!'};
         }
 
-        return {goban: goban, captured: captured};
+        return { goban: goban, captured: captured, error: undefined };
     };
 
     doMove = (x, y, pass = false) => {
-        // deep copy of current goban state
-        const goban = this.state.gobanHistory[this.state.move].map(gobanRow => gobanRow.slice());
-
-        // pass
-        if (pass) {
-            this.setState(state => {
-                return {
-                    move: state.move + 1,
-                    gobanHistory: state.gobanHistory.concat([goban]),
-                }
-            });
-            return;
-        }
-
-        // the place is taken
-        if (goban[x][y] !== undefined) return;
+        const goban = this.state.gobanHistory[this.state.move]
 
         // kill all the other stones around if needed
-        const {goban: newGoban, captured} = this.updateGoban(goban, x, y);
+        const { goban: newGoban, captured, error } = this.updateGoban(goban, this.state.move, x, y, pass);
 
-        if (newGoban === false) {
-            console.log('Suicide detected!');
+        if (error) {
+            console.log(error);
             return;
         }
 
@@ -100,28 +155,36 @@ class Game extends Component {
             };
         });
     }
-    
+
     render() {
         const next_move = (this.state.move % 2 === 0) ? 'черные' : 'белые'
+        let goban = null;
+        if (this.state.gobanSize > 0) {
+            goban = (
+                <>
+                    <p>Ход №{this.state.move}, ходят {next_move}</p>
+                    <p>В плену у черных: {this.state.capturedBlack}</p>
+                    <p>В плену у белых: {this.state.capturedWhite}</p>
+                    <Goban
+                        size={this.state.gobanSize}
+                        stones={this.state.gobanHistory[this.state.move]}
+                        move={this.state.move}
+                        onMove={this.doMove}
+                    />
+                    <div>
+                        <Button
+                            color="primary"
+                            variant="contained"
+                            onClick={() => this.doMove(null, null, true)}
+                        >Пас</Button>
+                    </div>
+                </>
+            );
+        }
 
         return (
             <div>
-                <p>Ход №{this.state.move}, ходят {next_move}</p>
-                <p>В плену у черных: {this.state.capturedBlack}</p>
-                <p>В плену у белых: {this.state.capturedWhite}</p>
-                <Goban
-                    size={this.state.gobanSize}
-                    stones={this.state.gobanHistory[this.state.move]}
-                    move={this.state.move}
-                    onMove={this.doMove}
-                />
-                <div>
-                    <Button
-                        color="primary"
-                        variant="contained"
-                        onClick={() => this.doMove(null, null, true)}
-                    >Пас</Button>
-                </div>
+                {goban}
             </div>
         );
     }
