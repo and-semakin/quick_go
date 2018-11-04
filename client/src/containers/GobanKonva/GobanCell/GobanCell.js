5915f13a53978cd07915a94d9a929f258019c0ff
Andrey Semakin <and-semakin@ya.ru>
2018-11-04 22:33:56 +0500

    Fix some ESLint errors

--- client/src/containers/GobanKonva/GobanCell/GobanCell.js
+++ client/src/containers/GobanKonva/GobanCell/GobanCell.js
@@ -1,127 +1,152 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import {
   Group, Line, Circle, Rect,
 } from 'react-konva';
 
 class GobanCell extends Component {
-    state = {
-      hovered: false,
-    }
+  state = {
+    hovered: false,
+  }
 
-    onMouseOver = () => {
-      if (this.props.stone === undefined && this.props.showHover) {
-        this.setState({ hovered: true });
-      }
+  onMouseOver = () => {
+    if (this.props.stone === undefined && this.props.showHover) {
+      this.setState({ hovered: true });
     }
+  }
 
-    onMouseLeave = () => {
-      if (this.props.stone === undefined && this.props.showHover) {
-        this.setState({ hovered: false });
-      }
-    }
-
-    onClick = () => {
+  onMouseLeave = () => {
+    if (this.props.stone === undefined && this.props.showHover) {
       this.setState({ hovered: false });
-      this.props.onClick();
     }
+  }
 
-    render() {
-      const size = this.props.size;
+  onClick = () => {
+    this.setState({ hovered: false });
+    this.props.onClick();
+  }
 
-      let hover = null;
-      let stone = null;
-      let recentMark = null;
+  render() {
+    const size = this.props.size;
 
-      if (this.state.hovered) {
-        hover = (
-          <Circle
-            preventDefault={false}
-            fill={(this.props.isHoverBlack) ? 'black' : 'white'}
-            opacity={0.5}
-            x={(size / 2) + 0.5}
-            y={(size / 2) + 0.5}
-            radius={(size / 2)}
-          />
-        );
-      }
+    let hover = null;
+    let stone = null;
+    let recentMark = null;
 
-      if (this.props.stone !== undefined) {
-        stone = (
-          <Circle
-            preventDefault={false}
-            fill={(this.props.stone % 2 === 0) ? 'black' : 'white'}
-            x={(size / 2) + 0.5}
-            y={(size / 2) + 0.5}
-            radius={(size / 2)}
-          />
-        );
-
-        if (this.props.isRecent) {
-          recentMark = (
-            <Circle
-              preventDefault={false}
-              stroke={(this.props.stone % 2 !== 0) ? 'black' : 'white'}
-              strokeWidth={1.5}
-              x={(size / 2) + 0.5}
-              y={(size / 2) + 0.5}
-              radius={(size / 2) * 0.6}
-            />
-          );
-        }
-      }
-
-      return (
-        <Group
+    if (this.state.hovered) {
+      hover = (
+        <Circle
           preventDefault={false}
-          x={this.props.x}
-          y={this.props.y}
-          height={size}
-          width={size}
-          onMouseEnter={this.onMouseOver}
-          onMouseLeave={this.onMouseLeave}
-          onClick={this.onClick}
-          onTap={this.onClick}
-        >
-          {/* for hover */}
-          <Rect
-            preventDefault={false}
-            x={0}
-            y={0}
-            height={size}
-            width={size}
-          />
-          {/* vertical */}
-          <Line
-            preventDefault={false}
-            points={[
-              (size / 2) + 0.5,
-              (this.props.isTop) ? (size / 2) + 0.5 : 0.5,
-              (size / 2) + 0.5,
-              // +1 чтобы не было зазора в полпикселя на стыке
-              (this.props.isBottom) ? (size / 2) + 0.5 : size + 1,
-            ]}
-            stroke="black"
-            strokeWidth={1}
-          />
-          {/* horizontal */}
-          <Line
-            preventDefault={false}
-            points={[
-              (this.props.isLeft) ? (size / 2) + 0.5 : 0.5,
-              (size / 2) + 0.5,
-              // +1 чтобы не было зазора в полпикселя на стыке
-              (this.props.isRight) ? (size / 2) + 0.5 : size + 1,
-              (size / 2) + 0.5,
-            ]}
-            stroke="black"
-            strokeWidth={1}
-          />
-          {hover}
-          {stone}
-          {recentMark}
-        </Group>
+          fill={(this.props.isHoverBlack) ? 'black' : 'white'}
+          opacity={0.5}
+          x={(size / 2) + 0.5}
+          y={(size / 2) + 0.5}
+          radius={(size / 2)}
+        />
       );
     }
+
+    if (this.props.stone !== undefined) {
+      stone = (
+        <Circle
+          preventDefault={false}
+          fill={(this.props.stone % 2 === 0) ? 'black' : 'white'}
+          x={(size / 2) + 0.5}
+          y={(size / 2) + 0.5}
+          radius={(size / 2)}
+        />
+      );
+
+      if (this.props.isRecent) {
+        recentMark = (
+          <Circle
+            preventDefault={false}
+            stroke={(this.props.stone % 2 !== 0) ? 'black' : 'white'}
+            strokeWidth={1.5}
+            x={(size / 2) + 0.5}
+            y={(size / 2) + 0.5}
+            radius={(size / 2) * 0.6}
+          />
+        );
+      }
+    }
+
+    return (
+      <Group
+        preventDefault={false}
+        x={this.props.x}
+        y={this.props.y}
+        height={size}
+        width={size}
+        onMouseEnter={this.onMouseOver}
+        onMouseLeave={this.onMouseLeave}
+        onClick={this.onClick}
+        onTap={this.onClick}
+      >
+        {/* for hover */}
+        <Rect
+          preventDefault={false}
+          x={0}
+          y={0}
+          height={size}
+          width={size}
+        />
+        {/* vertical */}
+        <Line
+          preventDefault={false}
+          points={[
+            (size / 2) + 0.5,
+            (this.props.isTop) ? (size / 2) + 0.5 : 0.5,
+            (size / 2) + 0.5,
+            // +1 чтобы не было зазора в полпикселя на стыке
+            (this.props.isBottom) ? (size / 2) + 0.5 : size + 1,
+          ]}
+          stroke="black"
+          strokeWidth={1}
+        />
+        {/* horizontal */}
+        <Line
+          preventDefault={false}
+          points={[
+            (this.props.isLeft) ? (size / 2) + 0.5 : 0.5,
+            (size / 2) + 0.5,
+            // +1 чтобы не было зазора в полпикселя на стыке
+            (this.props.isRight) ? (size / 2) + 0.5 : size + 1,
+            (size / 2) + 0.5,
+          ]}
+          stroke="black"
+          strokeWidth={1}
+        />
+        {hover}
+        {stone}
+        {recentMark}
+      </Group>
+    );
+  }
 }
+
+GobanCell.propTypes = {
+  size: PropTypes.number.isRequired,
+  isHoverBlack: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
+  stone: PropTypes.number.isRequired,
+  x: PropTypes.number.isRequired,
+  y: PropTypes.number.isRequired,
+  showHover: PropTypes.bool,
+  isRecent: PropTypes.bool,
+  isLeft: PropTypes.bool,
+  isRight: PropTypes.number,
+  isTop: PropTypes.number,
+  isBottom: PropTypes.number,
+};
+
+GobanCell.defaultProps = {
+  showHover: false,
+  isRecent: false,
+  isLeft: false,
+  isRight: false,
+  isTop: false,
+  isBottom: false,
+};
 
 export default GobanCell;
--- client/src/containers/GobanKonva/GobanCells/GobanCells.js
+++ client/src/containers/GobanKonva/GobanCells/GobanCells.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Group } from 'react-konva';
 
 import GobanCell from '../GobanCell/GobanCell';
 
 const gobanCells = (props) => {
-  const gobanCells = [];
+  const cells = [];
   const size = props.size;
   const border = size - 1;
   const cellSize = props.cellSize; // px
-  for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-      gobanCells.push(
+  for (let i = 0; i < size; i += 1) {
+    for (let j = 0; j < size; j += 1) {
+      cells.push(
         <GobanCell
           key={`${String(i)};${String(j)}`}
           preventDefault={false}
@@ -32,9 +33,21 @@ const gobanCells = (props) => {
   }
   return (
     <Group x={props.x} y={props.y}>
-      {gobanCells}
+      {cells}
     </Group>
   );
+};
+
+gobanCells.propTypes = {
+  size: PropTypes.number.isRequired,
+  cellSize: PropTypes.number.isRequired,
+  onMove: PropTypes.func.isRequired,
+  showHover: PropTypes.bool.isRequired,
+  isHoverBlack: PropTypes.bool.isRequired,
+  stones: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.number)).isRequired,
+  recentMove: PropTypes.arrayOf(PropTypes.number).isRequired,
+  x: PropTypes.number.isRequired,
+  y: PropTypes.number.isRequired,
 };
 
 export default gobanCells;
