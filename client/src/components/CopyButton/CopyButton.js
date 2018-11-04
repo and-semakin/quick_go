bb291f70af111a0fc6ef8a05375e0f8154b5b6dd
Andrey Semakin <and-semakin@ya.ru>
2018-11-04 22:43:41 +0500

    Fix some ESLint errors

--- client/src/components/CopyButton/CopyButton.js
+++ client/src/components/CopyButton/CopyButton.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import CheckIcon from '@material-ui/icons/Check';
@@ -13,6 +15,7 @@ function fallbackCopyTextToClipboard(text) {
   try {
     document.execCommand('copy');
   } catch (err) {
+    // eslint-disable-next-line no-console
     console.error('Fallback: Oops, unable to copy', err);
   }
 
@@ -28,44 +31,54 @@ function copyTextToClipboard(text) {
 }
 
 class CopyButton extends Component {
-    state = {
-      done: false,
-    }
+  state = {
+    done: false,
+  }
 
-    onClickHandler = () => {
-      copyTextToClipboard(this.props.value);
-      this.setState({ done: true });
-      setTimeout(() => {
-        this.setState({ done: false });
-      }, 1500);
-    }
+  onClickHandler = () => {
+    copyTextToClipboard(this.props.value);
+    this.setState({ done: true });
+    setTimeout(() => {
+      this.setState({ done: false });
+    }, 1500);
+  }
 
-    render() {
-      const button = (this.state.done)
-        ? (
-          <IconButton>
-            <CheckIcon fontSize="small" />
-          </IconButton>
-        )
-        : (
-          <Button
-            onClick={this.onClickHandler}
-          >
-            {this.props.children}
-          </Button>
-        );
-      return (
-        <div style={{
-          width: `${this.props.width}px`,
-          textAlign: 'center',
-          display: 'flex',
-          justifyContent: 'center',
-        }}
+  render() {
+    const button = (this.state.done)
+      ? (
+        <IconButton>
+          <CheckIcon fontSize="small" />
+        </IconButton>
+      )
+      : (
+        <Button
+          onClick={this.onClickHandler}
         >
-          {button}
-        </div>
+          {this.props.children}
+        </Button>
       );
-    }
+    return (
+      <div style={{
+        width: `${this.props.width}px`,
+        textAlign: 'center',
+        display: 'flex',
+        justifyContent: 'center',
+      }}
+      >
+        {button}
+      </div>
+    );
+  }
 }
+
+CopyButton.propTypes = {
+  value: PropTypes.string.isRequired,
+  width: PropTypes.number.isRequired,
+  children: PropTypes.node,
+};
+
+CopyButton.defaultProps = {
+  children: 'Copy',
+};
 
 export default CopyButton;
--- client/src/components/NewGame/NewGame.js
+++ client/src/components/NewGame/NewGame.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import ReactRouterPropTypes from 'react-router-prop-types';
+
 import Button from '@material-ui/core/Button';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
@@ -14,139 +16,144 @@ import CopyButton from '../CopyButton/CopyButton';
 import './NewGame.css';
 
 class NewGame extends Component {
-    state = {
-      gobanSize: '9',
-      gobanSizes: ['9', '13', '19'],
-      moveSubmitEnabled: true,
-      blackLink: '',
-      whiteLink: '',
-    }
+  state = {
+    gobanSize: '9',
+    gobanSizes: ['9', '13', '19'],
+    moveSubmitEnabled: true,
+    blackLink: '',
+    whiteLink: '',
+  }
 
-    handleGobanSizeChange = (event) => {
-      this.setState({ gobanSize: event.target.value });
-    };
+  handleGobanSizeChange = (event) => {
+    this.setState({ gobanSize: event.target.value });
+  };
 
-    handleButtonClick = () => {
-      const postData = new FormData();
-      postData.set('goban_size', this.state.gobanSize);
-      postData.set('move_submit_enabled', this.state.moveSubmitEnabled);
-      axios.post('/api/new', postData).then((response) => {
-        this.setState({
-          blackLink: response.data.link_black,
-          whiteLink: response.data.link_white,
-        });
-      }).catch((error) => {
-        console.log(error);
+  handleButtonClick = () => {
+    const postData = new FormData();
+    postData.set('goban_size', this.state.gobanSize);
+    postData.set('move_submit_enabled', this.state.moveSubmitEnabled);
+    axios.post('/api/new', postData).then((response) => {
+      this.setState({
+        blackLink: response.data.link_black,
+        whiteLink: response.data.link_white,
       });
-    }
+    }).catch((error) => {
+      // eslint-disable-next-line no-console
+      console.log(error);
+    });
+  }
 
-    handleSwitchToggle = name => (event) => {
-      this.setState({ [name]: event.target.checked });
-    }
+  handleSwitchToggle = name => (event) => {
+    this.setState({ [name]: event.target.checked });
+  }
 
-    render() {
-      const gobanSizesRadioButtons = this.state.gobanSizes.map(size => (
-        <FormControlLabel
-          key={size}
-          value={size}
-          control={<Radio />}
-          label={`${String(size)} x ${String(size)}`}
-        />
-      ));
+  render() {
+    const gobanSizesRadioButtons = this.state.gobanSizes.map(size => (
+      <FormControlLabel
+        key={size}
+        value={size}
+        control={<Radio />}
+        label={`${String(size)} x ${String(size)}`}
+      />
+    ));
 
-      const link_prefix = '/game/';
-      const full_link_prefix = document.location.origin + link_prefix;
+    const linkPrefix = '/game/';
+    const fullLinkPrefix = document.location.origin + linkPrefix;
 
-      let links = null;
-      if (this.state.blackLink) {
-        links = (
-          <div>
-            <div className="link-input">
-              <TextField
-                label="Black"
-                variant="outlined"
-                margin="dense"
-                value={full_link_prefix + this.state.blackLink}
-              />
-              <CopyButton
-                value={full_link_prefix + this.state.blackLink}
-                width={69}
-              >
-Copy
-              </CopyButton>
-              <Button onClick={() => {
-                this.props.history.push(link_prefix + this.state.blackLink);
-              }}
-              >
-Open
-              </Button>
-            </div>
-            <div className="link-input">
-              <TextField
-                label="White"
-                variant="outlined"
-                margin="dense"
-                value={full_link_prefix + this.state.whiteLink}
-              />
-              <CopyButton
-                value={full_link_prefix + this.state.whiteLink}
-                width={69}
-              >
-Copy
-              </CopyButton>
-              <Button onClick={() => {
-                this.props.history.push(link_prefix + this.state.whiteLink);
-              }}
-              >
-Open
-              </Button>
-            </div>
-          </div>
-        );
-      }
-
-      return (
+    let links = null;
+    if (this.state.blackLink) {
+      links = (
         <div>
-          <h1>Play Go</h1>
-          <div>
-            <FormControl component="fieldset">
-              <FormLabel component="legend">Goban Size</FormLabel>
-              <RadioGroup
-                aria-label="Goban size"
-                name="gobanSize"
-                value={this.state.gobanSize}
-                onChange={this.handleGobanSizeChange}
-              >
-                {gobanSizesRadioButtons}
-              </RadioGroup>
-
-              <FormLabel component="legend">Settings</FormLabel>
-              <FormGroup>
-                <FormControlLabel
-                  control={(
-                    <Switch
-                      checked={this.state.moveSubmitEnabled}
-                      onChange={this.handleSwitchToggle('moveSubmitEnabled')}
-                    />
-)}
-                  label="Submit move before send"
-                />
-              </FormGroup>
-            </FormControl>
-          </div>
-          <div>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={this.handleButtonClick}
+          <div className="link-input">
+            <TextField
+              label="Black"
+              variant="outlined"
+              margin="dense"
+              value={fullLinkPrefix + this.state.blackLink}
+            />
+            <CopyButton
+              value={fullLinkPrefix + this.state.blackLink}
+              width={69}
             >
-Start!
+              Copy
+            </CopyButton>
+            <Button onClick={() => {
+              this.props.history.push(linkPrefix + this.state.blackLink);
+            }}
+            >
+              Open
             </Button>
           </div>
-          {links}
+          <div className="link-input">
+            <TextField
+              label="White"
+              variant="outlined"
+              margin="dense"
+              value={fullLinkPrefix + this.state.whiteLink}
+            />
+            <CopyButton
+              value={fullLinkPrefix + this.state.whiteLink}
+              width={69}
+            >
+              Copy
+            </CopyButton>
+            <Button onClick={() => {
+              this.props.history.push(linkPrefix + this.state.whiteLink);
+            }}
+            >
+              Open
+            </Button>
+          </div>
         </div>
       );
     }
+
+    return (
+      <div>
+        <h1>Play Go</h1>
+        <div>
+          <FormControl component="fieldset">
+            <FormLabel component="legend">Goban Size</FormLabel>
+            <RadioGroup
+              aria-label="Goban size"
+              name="gobanSize"
+              value={this.state.gobanSize}
+              onChange={this.handleGobanSizeChange}
+            >
+              {gobanSizesRadioButtons}
+            </RadioGroup>
+
+            <FormLabel component="legend">Settings</FormLabel>
+            <FormGroup>
+              <FormControlLabel
+                control={(
+                  <Switch
+                    checked={this.state.moveSubmitEnabled}
+                    onChange={this.handleSwitchToggle('moveSubmitEnabled')}
+                  />
+                )}
+                label="Submit move before send"
+              />
+            </FormGroup>
+          </FormControl>
+        </div>
+        <div>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={this.handleButtonClick}
+          >
+            Start!
+          </Button>
+        </div>
+        {links}
+      </div>
+    );
+  }
 }
+
+NewGame.propTypes = {
+  history: ReactRouterPropTypes.history.isRequired,
+};
 
 export default NewGame;
